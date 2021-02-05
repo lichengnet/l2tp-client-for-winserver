59762c87e533454ae6d2e75cd7cbb6b3947289b9
licheng <lichengnet@gmail.com>
2021-02-05 16:23:34 +0800

    +sockd支持

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
 FROM centos:centos7.9.2009
 
+ENV SCOKS5_ENABLE 0
+ENV SCOKS5_FORKS 2
+ENV SCOKS5_START_DELAY 2
+
 RUN yum -y install epel-release net-tools \
     && yum --enablerepo=epel -y install strongswan xl2tpd \
     && yum -y install strongswan xl2tpd \
     && yum clean all \
     && mkdir -p /var/run/xl2tpd
+
+COPY *.rpm /
+RUN rpm -Uvh *.rpm
 
 # VPN Files
 COPY ipsec.conf /etc/ipsec.conf
@@ -12,13 +19,17 @@ COPY ipsec.secrets /etc/ipsec.secrets
 COPY xl2tpd.conf /etc/xl2tpd/xl2tpd.conf
 COPY options.l2tpd.client /etc/ppp/options.l2tpd.client
 
-# Scripts
+# l2tp Files
 RUN chmod 600 /etc/ipsec.secrets
 RUN chmod 600 /etc/ppp/options.l2tpd.client
 RUN mv /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.conf.old 2>/dev/null
 RUN mv /etc/strongswan/ipsec.secrets /etc/strongswan/ipsec.secrets.old 2>/dev/null
 RUN ln -s /etc/ipsec.conf /etc/strongswan/ipsec.conf
 RUN ln -s /etc/ipsec.secrets /etc/strongswan/ipsec.secrets
+
+# Socks5 Files
+COPY sockd.conf /etc/sockd.conf
+
 COPY startup.sh /
 COPY reconnector.sh /
 RUN chmod +x /startup.sh
--- reconnector.sh
+++ reconnector.sh
@@ -2,18 +2,30 @@
 while true; do
   if ! route | grep  ppp0 > /dev/null; then
     # strongswan down myvpn
-    echo "reconnector: wait for 5 secs";sleep 5
+    echo "connector: wait for 3 secs";sleep 3
     strongswan up myvpn
     # wait for 5 sec to make sure ppp0 is avalible again in case of diconnection
     # try to connect vpn if it's not connected
     xl2tpd-control -c /var/run/xl2tpd/l2tp-control connect 'myvpn'
     # Wait for vpn to create ppp0 network interface
-    while ! route | grep ppp0 > /dev/null; do echo "reconnector: waiting for ppp0"; sleep 1; done
+    while ! route | grep ppp0 > /dev/null; do echo "checking: waiting for ppp0"; sleep 1; done
 
     #add route
     route add -net ${VPN_ROUTE} dev ppp0
     ifconfig |grep inet
     route
+
+    ##restart socks5
+    if [[ $SCOKS5_ENABLE -eq 1 ]];then
+        # kill sockd
+        echo "startup/socks5: waiting for ppp0"
+      (while ! route | grep ppp0 > /dev/null; do sleep 1; done \
+        && echo "startup/socks5: Socks5 will start in $SCOKS5_START_DELAY seconds" \
+        && sleep $SCOKS5_START_DELAY \
+        && sockd -N $SCOKS5_FORKS) &
+    else
+      echo "startup/socks5: Ignore socks5 server."
+    fi
   else
     sleep 10
   fi
